3b31fb3046e2866d76e5d6547b10a49adcdea865
Jason Bauzon <jasonbauzon@gmail.com>
2022-08-12 23:19:35 -0500

    add eventlistener for multiply

--- script.js
+++ script.js
@@ -2,55 +2,69 @@ const display = document.querySelector('#display');
 const one = document.querySelector('.one');
 const two = document.querySelector('.two')
 const add = document.querySelector('.add')
+const multiply = document.querySelector('.multiply')
 let operator = '';
 const equals = document.querySelector('.equals')
 let displayValue = 0;
 let numbers = [];
-// let secondNumber = [];
 
 const operate = () => {
     switch (operator) {
     case '+':
         if (numbers.length == 2) {
             display.innerText = numbers[0]+numbers[1];
-            // displayValue = [display.innerText];
             numbers = [parseInt(display.innerText)];
-            console.log(`numbers is ${numbers}`);
-            // secondNumber.shift();    
         }
         break;
+    case 'x':
+        if (numbers.length == 2) {
+            display.innerText = numbers[0]*numbers[1];
+            numbers = [parseInt(display.innerText)];
+            console.log(numbers);
+        }
+        break;
+
 }};
 
-// event listners
+// event listeners
 one.addEventListener('click', () => {
-    displayValue == 0 ? displayValue = one.innerText : displayValue += (one.innerText);
-    console.log(displayValue);
+    displayValue == 0 ? displayValue = '1' : displayValue += '1';
     display.innerText = displayValue;
 });
 two.addEventListener('click', () => {
-    displayValue == 0 ? displayValue = two.innerText : displayValue += (two.innerText);
-    console.log(displayValue);
+    displayValue == 0 ? displayValue = '2' : displayValue += '2';
     display.innerText = displayValue;
 });
 add.addEventListener('click', () => {
     if (numbers[0] == undefined)  {
         numbers.push(parseInt(displayValue))
         operator = add.innerText;
-        console.log(numbers);
         displayValue = 0    
     } else if (operator != undefined) {
         operator = add.innerText;
         numbers.push(parseInt(displayValue));
-        console.log(`numbers is ${numbers}`);
         displayValue = 0
         operate();    
     }
 });
+multiply.addEventListener('click', () => {
+    if (numbers[0] == undefined)  {
+        numbers.push(parseInt(displayValue))
+        console.log(numbers);
+        operator = multiply.innerText;
+        displayValue = 0    
+    } else if (operator != undefined) {
+        operator = multiply.innerText;
+        numbers.push(parseInt(displayValue));
+        console.log(numbers);
+        displayValue = 0
+        operate();    
+    }
+});
+
 equals.addEventListener('click', () => {
     numbers.push(parseInt(displayValue));
-    console.log(numbers);
     displayValue = 0
     operate();
 });
 
-// use ONE array to store the numbers?
